ad60b3c8acbbd10da21714addde26b07553c503d
Lucas Barros <lbarros.comp@gmail.com>
2021-08-05 21:55:27 -0300

    Updating the title and vars

--- components/Steps/Step1/index.tsx
+++ components/Steps/Step1/index.tsx
@@ -2,13 +2,8 @@ import { useEffect, useState } from 'react';
 import Hero from '../../../components/Hero'
 
 export default function Step1() {
-  // Como salvar a pontuacao dele para o calculo final?
-  const [title, setTitle] = useState<string>('Olá, eu sou o Descomplikator');
-  const [subtitle, setSubtitle] = useState<string>('Prepare-se para descobrir grandes coisas sobre o seu futuro!');
-  const [imageSize] = useState('big');
-
   return (
-    <Hero title={title} subtitle={subtitle} imageSize={imageSize}>
+    <Hero title="Olá, eu sou o Descomplikator" subtitle="Prepare-se para descobrir grandes coisas sobre o seu futuro!" imageSize="big">
       {/* Componentes aqui */}
     </Hero>
   )
